535ed3cf82d35906054d081f0f379d0525bbf4f8
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-01-08 19:05:23 -0800

    connected to firebase db and created functionality to post messages to post section of app

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -8,18 +8,35 @@ import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Posts from "./Posts";
 import { db } from "../firebase";
+import firebase from "firebase";
 
 function Feed() {
+  const [input, setInput] = useState("");
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapShot(snapshot => (
-      setPosts()
-    ))
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
   }, []);
 
   const sendPost = (e) => {
     e.preventDefault();
+    db.collection("posts").add({
+      name: "Paul Gonzalez",
+      description: "this is a test",
+      message: input,
+      photo: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
   };
 
   return (
@@ -28,7 +45,11 @@ function Feed() {
         <div className="feed__input">
           <CreateIcon />
           <form action="">
-            <input type="text" />
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
             <button onClick={sendPost} type="submit">
               Send
             </button>
@@ -45,14 +66,15 @@ function Feed() {
           />
         </div>
       </div>
-      {posts.map((post) => (
-        <Posts />
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Posts
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
       ))}
-      <Posts
-        name="Paul Gonzalez"
-        description="this is a test"
-        message="wow this is working"
-      />
     </div>
   );
 }
